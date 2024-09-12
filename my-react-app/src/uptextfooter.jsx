05ec5372baed4e26cdd7e43cb004a721f356df0d
MarsDVG <134861102+MarsDVG@users.noreply.github.com>
2024-09-12 22:43:49 +0300

    feat: add new section class

--- my-react-app/src/uptextfooter.jsx
+++ my-react-app/src/uptextfooter.jsx
@@ -6,26 +6,26 @@ class UpTextFooter extends Component {
     return (
 <div class="uptextfooter">
 <div class="contactus">
-    <h3 id="svjaz"></h3>
+    <h3 id="svjaz">CONNECT WITH US</h3>
     <div class="textcontactus">
-        <p id="chtob"></p>
-        <p id="dlya"></p>
-        <p id="tek"></p>
+        <p id="chtob">To get in touch, visit our</p>
+        <p id="dlya">. For employment</p>
+        <p id="tek">current vacancies.</p>
         <a href="https://mail.google.com/mail/u/0/#inbox">VladNeckt@gmail.com</a>
-        <a href="tel:+375296081350" id="poz"></a>
+        <a href="tel:+375296081350" id="poz">Call</a>
         <a href="https://github.com/DoniZefironi">github</a>
-        <a href="#" id="openkart"></a>
+        <a href="#" id="openkart">Open map</a>
     </div>
 </div>
 <div class="opening">
-    <h3 id="chas"></h3>
+    <h3 id="chas">OPENING HOURS</h3>
     <div class="textopening">
-        <p id="vtornkib1"></p>
-        <p id="pat1"></p>
+        <p id="vtornkib1">Tuesday–Thursday from 16:00 to 23:00</p>
+        <p id="pat1">Friday and Saturday from 12:00 to late night</p>
     </div>
 </div>
 <div class="socials">
-    <h3 id="socialen"></h3>
+    <h3 id="socialen">SOCIALS</h3>
     <p>INSTAGRAM</p>
     <p>FACEBOOK</p>
 </div>
